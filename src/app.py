6a8e540e3f41c932cad528b3d521e2d280e4ec6c
DESKTOP-M0T6E0V\Alvaro <alvaro.martin.salazar@iesciudadjardin.com>
2023-12-16 12:27:36 +0100

    buscador y muestra en pagina cualquier web

--- src/app.py
+++ src/app.py
@@ -61,6 +61,58 @@ def login():
         # Si la solicitud es GET, muestra el formulario de inicio de sesión
         return render_template('login.html')
     
+@app.route('/index', methods=['GET', 'POST'])
+def web():
+    enlaces_principales=[]
+    enlaces_secundarios=[]
+    if request.method == 'POST':
+
+        url = request.form['web']
+
+        # Verificar si la URL tiene el esquema (http:// o https://)
+        if not (url.startswith('http://') or url.startswith('https://')):
+            url = 'http://' + url  # Agregar el esquema http:// si no está presente
+
+        try:
+            html = urllib.request.urlopen(url)
+            soup = BeautifulSoup(html, 'html.parser')
+
+            tags = soup.find_all('a')
+
+            print('Enlaces en la página principal: \r\n')
+
+            for tag in tags:
+                enlaces_principales.append({'texto': tag.contents[0], 'url': tag.get('href')})
+
+            print('\r\n Enlaces en las páginas secundarias: \r\n')
+
+            for tag in tags:
+                enlaces_secundarios.append({'url': newtag.get('href')})
+                print('Accediendo a las páginas de la web', enlaces_secundarios)
+                try:
+                    if enlaces_secundarios and (enlaces_secundarios.startswith('http://') or enlaces_secundarios.startswith('https://')):
+                        html2 = urllib.request.urlopen(enlaces_secundarios)
+                    else:
+                        if enlaces_secundarios:
+                            html2 = urllib.request.urlopen(url + enlaces_secundarios)
+                        else:
+                            continue  # Ignorar enlaces nulos
+                    soup2 = BeautifulSoup(html2, 'html.parser')
+                    newtags = soup2.find_all('a')
+                    if len(newtags) > 0:
+                        print(len(newtags), 'enlaces: ')
+                        for newtag in newtags:
+                            print(newtag.get('href'))
+                    else:
+                        print('No hay más enlaces')
+                except Exception as e:
+                    print('Algo ha fallado:', e)
+
+        except Exception as e:
+            print('Error al abrir la URL:', e)
+
+    return render_template('index.html', enlaces_principales=enlaces_principales, enlaces_secundarios=enlaces_secundarios)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
