7cfd3406290e3d9873b401ef34f1cfca13f52136
alvaromartinsalazar <alvaromartinsalazar@gmail.com>
2023-12-18 14:20:05 +0100

    buscador

--- src/app.py
+++ src/app.py
@@ -9,22 +9,6 @@ template_dir = os.path.join(template_dir, 'src', 'templates')
 
 app = Flask(__name__, template_folder = template_dir)
 
-"""url = 'https://www.pisos.com/viviendas/sevilla/'
-html = urllib.request.urlopen(url)
-soup = BeautifulSoup(html)
-url_principal='https://www.pisos.com'
-
-tags=soup('div')
-zonas = soup.find(class_='zoneList')
-
-for tag in tags:
-    tags2=zonas('a')
-    for tag2 in tags2:
-        enlaces_zonas=tag2.get('href')
-        urls=url_principal+enlaces_zonas
-        print(urls)"""
-
-
 @app.route('/')
 def home():
     cursor = db.database.cursor()
